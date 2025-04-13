347a04d26eab22dcfae2732bff078f25a0ba582c
arsh-codes <11322brar@gmail.com>
2025-04-13 15:47:54 +0000

    added firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5Aq0DkWU1jbPOVwnOYWEr420iDwsIRPg",
+  authDomain: "herm-chat.firebaseapp.com",
+  projectId: "herm-chat",
+  storageBucket: "herm-chat.firebasestorage.app",
+  messagingSenderId: "1030666107417",
+  appId: "1:1030666107417:web:cd388b6fc32909ae2d4888",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
